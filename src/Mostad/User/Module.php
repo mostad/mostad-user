9f3bdc39aa286e65e1950527523a1f4d87c8cf9f
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2015-02-03 01:53:51 +0100

    Missing slash

--- src/Mostad/User/Module.php
+++ src/Mostad/User/Module.php
@@ -12,6 +12,6 @@ class Module
      */
     public function getConfig()
     {
-        return (array) require __DIR__ .'../../../config/module.config.php';
+        return (array) require __DIR__ .'/../../../config/module.config.php';
     }
 }
