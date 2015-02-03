3fae746d17ad94de824bcfe54c6b4e4ac5516b14
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2015-02-03 01:17:46 +0100

    Initial commit

---
+++ config/module.config.php
@@ -0,0 +1,6 @@
+<?php
+namespace Mostad\User;
+
+return [
+
+];
---
+++ src/Mostad/User/Module.php
@@ -0,0 +1,17 @@
+<?php
+namespace Mostad\User;
+
+/**
+ * Class Module
+ * @package Mostad\User
+ */
+class Module
+{
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return (array) require __DIR__ .'../../../config/module.config.php';
+    }
+}
