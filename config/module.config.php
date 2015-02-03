f90b0b97aa77f8ea3304d6c0920fee787dc4ec64
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2015-02-03 03:17:46 +0100

    Working authorization

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,43 @@
 <?php
 namespace Mostad\User;
 
-return [
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Mostad\User\Entity\User;
+use Zend\Authentication\AuthenticationService;
+use ZfrOAuth2\Server\Entity\TokenOwnerInterface;
+use ZfrOAuth2Module\Server\Factory\AuthenticationServiceFactory;
 
+return [
+    'doctrine' => [
+        'driver' => [
+            __NAMESPACE__ .'\Entity' => [
+                'class' => AnnotationDriver::class,
+                'paths' => __DIR__ .'/../src/'. str_replace('\\', '/', __NAMESPACE__) .'/Entity',
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ .'\Entity'  => __NAMESPACE__ .'\Entity',
+                ],
+            ],
+        ],
+
+        'entity_resolver' => [
+            'orm_default' => [
+                'resolvers' => [
+                    TokenOwnerInterface::class => User::class,
+                ],
+            ],
+        ],
+    ],
+
+    'service_manager' => [
+        'factories' => [
+            AuthenticationService::class => AuthenticationServiceFactory::class,
+        ],
+    ],
+
+    'zfr_oauth2_server' => [
+        'object_manager' => EntityManager::class,
+    ],
 ];
--- src/Mostad/User/Entity/User.php
+++ src/Mostad/User/Entity/User.php
@@ -1,20 +1,32 @@
 <?php
 namespace Mostad\User\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use ZfrOAuth2\Server\Entity\TokenOwnerInterface;
+
 /**
  * Class User
  *
  * @package Mostad\User\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="user")
  */
-class User implements UserInterface
+class User implements UserInterface, TokenOwnerInterface
 {
     /**
      * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Id
      */
     protected $id;
 
     /**
      * @var string
+     *
+     * @ORM\Column(nullable=false, type="string", unique=true)
      */
     protected $email;
 
@@ -40,5 +52,13 @@ class User implements UserInterface
     public function setEmail($email)
     {
         $this->email = $email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTokenOwnerId()
+    {
+        return $this->getId();
     }
 }
