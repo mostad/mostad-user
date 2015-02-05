ac76f64e3802d7118de8b9dd1f092a2339a8ca6d
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2015-02-05 19:08:24 +0100

    Another invalid namespace

--- config/module.config.php
+++ config/module.config.php
@@ -1,8 +1,8 @@
 <?php
 namespace Mostad\User;
 
-use Mostad\Service\UserService;
 use Mostad\User\Factory\Service\UserServiceFactory;
+use Mostad\User\Service\UserService;
 
 return [
     'service_manager' => [
